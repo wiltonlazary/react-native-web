ed806ee8722e3ebd2be515af7403cda8891d0523
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-01-01 14:12:30 -0800

    Exploration: repo design

---
+++ packages/react-native-web/src/exports/ViewPropTypes/index.js
@@ -0,0 +1,3 @@
+import ViewPropTypes, { type ViewProps, type ViewLayout, type ViewLayoutEvent } from '../View/ViewPropTypes';
+export type { ViewProps, ViewLayout, ViewLayoutEvent };
+export default ViewPropTypes;
